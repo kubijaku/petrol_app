7b6c151ab5975c024f19f0da66ebf634bbcd9e75
kubijaku <jakubkubicki@onet.pl>
2022-07-16 13:31:27 +0200

    creating ready structers to use in app

--- data_from_website/data.py
+++ data_from_website/data.py
@@ -12,33 +12,41 @@ class Data:
 
     Attributes
     ----------
-    Fuel : dict[int,string]
-        list containing price for the fuel and status (down or up)
-    region_data : dict[Name,Fuel]
+
+    Region_data : dict[Region_Name,Fuel]
         list conatining name of the region and Fuel
 
+    Fuel : dict[Fuel_Type_Name,[int,string]]
+        list containing price for the fuel and its status (down or up)
 
     """
+    Fuel = dict()
+    Region_data = dict()
+
     General_URL = 'https://www.autocentrum.pl/paliwa/ceny-paliw/'
 
     page = get(General_URL)
     bs = BeautifulSoup(page.content,features="html.parser")
-
+    Fuel_Type_Names = [ "95", "98", "ON", "ON+", "LPG" ]
 
     for all_regions in bs.find_all('tbody'):            #seperating whole file to get all regions
-        for region in all_regions.find_all('tr'):       #seperating get all regions for each region
+        for region in all_regions.find_all('tr'):       #seperating all regions for each region
             name = region.find('a',class_='row-link').get_text()
-            print(name)
+            # print(name)
 
             all_prices = region.find_all('td', class_='text-center')
-            for price in all_prices:
+            for index, price in enumerate(all_prices):
                 if (price.find(class_='down')):
                     status = "down"
                 elif(price.find(class_='up')):
                     status = "up"
                 else:
                     status = ""
-                print(price.get_text().strip(),status)  #printing price of petrol and it acutal status
+                # print(price.get_text().strip(),status)  #printing price of petrol and it acutal status
+                Fuel[ Fuel_Type_Names[ index ] ] = [ price.get_text().strip(), status]
+            Region_data[ name ] = Fuel
+
+
 
 
 
