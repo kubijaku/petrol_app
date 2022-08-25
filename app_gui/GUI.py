d348aa662d923cffc480a3ab69e09ab97964dd8f
kubijaku <jakubkubicki@onet.pl>
2022-08-25 11:12:15 +0200

    converting photo and adding as background

--- app_gui/GUI.py
+++ app_gui/GUI.py
@@ -6,7 +6,10 @@ root.geometry('880x400')
 title = Label(root, text='Petrol App')
 title.pack()
 
-bg = PhotoImage( file="")
- # tkinter.Label(root, text='app')
+bg = PhotoImage( file="/Users/jakubkubicki/PycharmProjects/petrol_app/images/petrol_background.png")
+Label(root, text='app')
+
+photo_label = Label( root, image=bg )
+photo_label.place( x=0, y=0, relwidth=1, relheight=1)
 
 root.mainloop()
