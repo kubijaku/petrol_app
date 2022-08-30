610f4d41c33dd8e00a951088da0682cd566f91bd
kubijaku <jakubkubicki@onet.pl>
2022-08-30 21:46:27 +0200

    converting photo and adding as background#2 changing GUI.py app - showing dynamically resizing photo and "moving" text

--- app_gui/GUI.py
+++ app_gui/GUI.py
@@ -1,15 +1,24 @@
 from tkinter import *
+from PIL import Image, ImageTk
+
 
 root = Tk()
 root.geometry('880x400')
 
-title = Label(root, text='Petrol App')
-title.pack()
+bg = ImageTk.PhotoImage( file="/Users/jakubkubicki/PycharmProjects/petrol_app/images/myimage.gif")
 
-bg = PhotoImage( file="/Users/jakubkubicki/PycharmProjects/petrol_app/images/petrol_background.png")
-Label(root, text='app')
+base_canvas = Canvas(root, width=880, height=400)
+base_canvas.pack(fill="both", expand=True, )
+base_canvas.create_image(0,0, image=bg, anchor="nw")
 
-photo_label = Label( root, image=bg )
-photo_label.place( x=0, y=0, relwidth=1, relheight=1)
+def resizer(e):
+    global bg1, resized_bg, new_bg
+    bg1 = Image.open("/Users/jakubkubicki/PycharmProjects/petrol_app/images/myimage.gif")
+    resized_bg = bg1.resize((e.width, e.height) , Image.Resampling.LANCZOS)
+    new_bg = ImageTk.PhotoImage(resized_bg)
+    base_canvas.create_image(10, 10, anchor=NW, image=new_bg)
+    base_canvas.create_text(int(e.width/2), int(e.height/2), text="Petrol App")
 
+
+root.bind('<Configure>', resizer)
 root.mainloop()
