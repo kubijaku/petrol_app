8131cfe09b7602d82f674555c599a9f9bb5a7883
kubijaku <jakubkubicki@onet.pl>
2022-09-03 15:50:01 +0200

    changing title's font and adding file with available fonts - fonts.py

--- app_gui/GUI.py
+++ app_gui/GUI.py
@@ -13,7 +13,7 @@ base_canvas.create_image(0,0, image=bg, anchor="nw")
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 text_size = min(screen_width,screen_height)/10
-base_canvas.create_text(440, 50, text="Petrol App", tags="TITLE", font=('Helvetica', int(text_size)))
+base_canvas.create_text(440, 50, text="Petrol App", tags="TITLE", font=('Apple Chancery', int(text_size),'italic'))
 
 def resizer(e):
     global bg1, resized_bg, new_bg
@@ -23,7 +23,7 @@ def resizer(e):
     base_canvas.create_image(10, 10, anchor=NW, image=new_bg)
     base_canvas.delete("TITLE")
     text_size = min(e.width, e.height)/10
-    base_canvas.create_text(int(e.width/2), int(e.height/8), text="Petrol App", tags="TITLE", font=('Helvetica', int(text_size)))
+    base_canvas.create_text(int(e.width/2), int(e.height/8), text="Petrol App", tags="TITLE", font=('Apple Chancery', int(text_size), 'italic'))
 
 
 root.bind('<Configure>', resizer)
